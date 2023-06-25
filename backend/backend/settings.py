1c1cf8ef446aa75f5b4c3d677ca1bb0f6184eedd
Есаулов Даниил <heigardev@yandex.ru>
2023-06-25 18:50:47 +0300

    ревьюэ

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES = { 
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'django'),
